15335eefe3c1d9224612b403a37e9ced1fc154cd
james.bobmin <james.bobmin@kittyvelour.com.au>
2024-09-23 18:27:17 +0800

    Add Netlify and RSS dependencies, and configure Astro for Netlify deployment
    - Added "@astrojs/netlify" and "@astrojs/rss" dependencies to package.json
    - Configured Astro to use Netlify adapter in astro.config.mjs
    - Created rss.xml.js file for generating RSS feed
    - Updated tags/index.astro to display tags with styling
    - Updated MarkdownPostLayout.astro to display publication date, author, and tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://classy-pothos-1028a3.netlify.app/",
+  output: "server",
+  adapter: netlify({ edgeMiddleware: true }),
+});
